bccf36394343b38ea31b069f515dc19d176ea23e
Gangbaekho <roorooro9933@gmail.com>
2020-09-09 14:29:20 +0900

    spring-boot init

---
+++ src/main/java/com/jinsoo/mongodbinspringboot/MongodbInSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jinsoo.mongodbinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbInSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbInSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinsoo/mongodbinspringboot/MongodbInSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsoo.mongodbinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbInSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
